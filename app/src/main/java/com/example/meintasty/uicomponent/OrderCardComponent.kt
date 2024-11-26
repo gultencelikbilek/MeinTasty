cc4796681dd28827923fb1575e17e1140973060d
gultencelikbilek <81778970+gultencelikbilek@users.noreply.github.com>
2024-11-27 01:19:41 +0300

    update order and basket screen

--- app/src/main/java/com/example/meintasty/uicomponent/OrderCardComponent.kt
+++ app/src/main/java/com/example/meintasty/uicomponent/OrderCardComponent.kt
@@ -1,6 +1,10 @@
 package com.example.meintasty.uicomponent
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,49 +12,113 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import com.example.meintasty.R
+import com.example.meintasty.domain.model.get_order_model.get_order_response.Order
 
 @Composable
 fun OrderCardComponent(
     modifier: Modifier = Modifier,
-  //  order: Order
+    order: Order
 ) {
-    //Text(text = order.name.toString())
-
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
+            .padding(horizontal = 16.dp)
+            .padding(top = 16.dp)
+            .height(150.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(1.dp)
     ) {
         Column {
             Row(
                 modifier = modifier.fillMaxWidth()
             ) {
-                Image(painter = painterResource(
-                    id = R.drawable.food_one),
-                    contentDescription = ""
-                )
+                Box(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .align(Alignment.CenterVertically)
+                        .border(
+                            width = 1.dp,
+                            color = Color.Transparent,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(4.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.food_one),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .height(85.dp)
+                            .width(85.dp)
+                    )
+                }
                 Column(
-                    modifier = Modifier.padding(top = 6.dp)
+                    modifier = Modifier
+                        .padding(top = 6.dp)
+                        .padding(start = 6.dp)
                 ) {
                     Row {
-                      //  Text(text = order.name.toString())
-                      //  Spacer(modifier = Modifier.width(8.dp))
-                      //  Text(text = order.price.toString())
+                        Text(
+                            text = order.name.toString(), style = TextStyle(
+                                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                                color = Color.Black
+                            )
+                        )
+                        Spacer(modifier = Modifier.width(50.dp))
+                        Text(
+                            text = order.price.toString(),
+                            style = TextStyle(
+                                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                                color = Color.Black
+                            )
+                        )
                     }
-                    //Text(text = order.orderDate.toString())
+                    Text(
+                        text = order.orderDate.toString(),
+                        style = TextStyle(
+                            fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                            color = Color.Black
+                        )
+                    )
                 }
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(id = R.string.repeat_order))
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.mein_tasty_color)
+                )
+            ) {
+                Text(
+                    text = stringResource(id = R.string.repeat_order),
+                    style = TextStyle(
+                        fontSize = MaterialTheme.typography.titleSmall.fontSize,
+                        color = Color.White
+                    )
+                )
             }
         }
     }
